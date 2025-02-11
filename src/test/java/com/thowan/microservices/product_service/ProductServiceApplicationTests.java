3a02568fde14637b9360838f2d02b3b45888e99d
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-02-11 20:24:52 +0700

    final

--- src/test/java/com/thowan/microservices/product_service/ProductServiceApplicationTests.java
+++ src/test/java/com/thowan/microservices/product_service/ProductServiceApplicationTests.java
@@ -1,15 +1,59 @@
 package com.thowan.microservices.product_service;
 
+import java.math.BigDecimal;
+
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
+
+import com.thowan.microservices.product_service.dto.ProductRequest;
+
+import io.restassured.RestAssured;
 
 @Import(TestcontainersConfiguration.class)
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+    @LocalServerPort
+    private Integer port;
+
+    @BeforeEach
+    void setup() {
+        RestAssured.baseURI = "http://localhost";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+        ProductRequest productRequest = getProductRequest();
+
+        RestAssured.given()
+                .contentType("application/json")
+                .body(productRequest)
+                .when()
+                .post("/api/product")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("name", Matchers.equalTo(productRequest.name()))
+                .body("description", Matchers.equalTo(productRequest.description()))
+                .body("price", Matchers.is(productRequest.price().intValueExact()));
+    }
+
+    private ProductRequest getProductRequest() {
+        return new ProductRequest("1" ,"iPhone 13", "iPhone 13", BigDecimal.valueOf(1200));
+    }
 
 }
