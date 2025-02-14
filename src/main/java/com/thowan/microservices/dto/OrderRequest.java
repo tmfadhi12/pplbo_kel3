20dea06f329ad05ebc84d5197f9c9b951122140b
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-02-14 10:23:55 +0700

    reset

--- src/main/java/com/thowan/microservices/dto/OrderRequest.java
+++
@@ -1,6 +0,0 @@
-package com.thowan.microservices.dto;
-
-import java.math.BigDecimal;
-
-public record OrderRequest(Long id,String orderNumber, String skuCode,BigDecimal price,Integer quantity) {
-}
--- src/main/java/com/thowan/microservices/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.thowan.microservices.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.thowan.microservices.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    
-} 
