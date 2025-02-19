607602dfaca6998c92aca05509a35aebd681b103
tmfadhli12 <thoriqmf123@gmail.com>
2025-02-19 14:53:38 +0700

    final

---
+++ src/main/java/com/thowan/microservices/api_gateway/routes/Routes.java
@@ -0,0 +1,34 @@
+package com.thowan.microservices.api_gateway.routes;
+
+import org.springframework.cloud.gateway.server.mvc.handler.GatewayRouterFunctions;
+import org.springframework.cloud.gateway.server.mvc.handler.HandlerFunctions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.function.RequestPredicates;
+import org.springframework.web.servlet.function.RouterFunction;
+import org.springframework.web.servlet.function.ServerResponse;
+
+@Configuration
+public class Routes {
+    
+    @Bean
+    public RouterFunction<ServerResponse> productServiceRoute(){
+        return GatewayRouterFunctions.route("product-service")
+        .route(RequestPredicates.path("/api/product"), HandlerFunctions.http("http://localhost:8080"))
+        .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> orderService(){
+        return GatewayRouterFunctions.route("order-service")
+        .route(RequestPredicates.path("/api/order"), HandlerFunctions.http("http://localhost:8081"))
+        .build();
+    }
+
+    @Bean
+    public RouterFunction<ServerResponse> inventoryService(){
+        return GatewayRouterFunctions.route("inventory-service")
+        .route(RequestPredicates.path("/api/inventory"), HandlerFunctions.http("http://localhost:8082"))
+        .build();
+    }
+}
