ccc6cdeb41ace09dcafca3ea15cb17090f58b34b
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-02-25 21:25:13 +0700

    part 9

--- src/main/java/com/thowan/microservices/client/InventoryClient.java
+++ src/main/java/com/thowan/microservices/client/InventoryClient.java
@@ -1,11 +1,24 @@
 package com.thowan.microservices.client;
 
+import org.slf4j.Logger;
+
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
     
 
 public interface InventoryClient {
-    
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
     @GetExchange("/api/inventory")
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
+
+    default boolean fallbackMethod(String code, Integer quantity, Throwable throwable) {
+        log.info("Cannot get inventory for skuCode {}, failure reason: {}",code, throwable.getMessage());
+        return false;
+    }
 }
