eb352a8c3e77d0b4633e6379a19d112760f26c6a
FahriYuwan <130884349+FahriYuwan@users.noreply.github.com>
2025-02-26 20:25:14 +0700

    part-10

--- src/main/java/com/thowan/microservices/product_service/dto/ProductResponse.java
+++
@@ -1,6 +0,0 @@
-package com.thowan.microservices.product_service.dto;
-
-import java.math.BigDecimal;
-
-public record ProductResponse(String id, String name, String description, BigDecimal price) {
-}
